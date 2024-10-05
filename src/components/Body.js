14cfa1a3d65fc0c5ef93fd0045b8b4cf4d181027
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-10-05 23:22:14 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,20 @@
 
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
+
 const Body=()=>{
+    const listOfRest=[];
     return(
  <div className="body">
-<div className="search">Search</div>
+<div className="filter">
+    <button className="filter-btn" onClick={()=>{console.log("Button Clicked")
+
+    }}
+  
+    >
+        Top Rated Restaurants
+        </button>
+</div>
 <div className="res-container">
   { resList.map(resturant=>
   <ResturantCard key={resturant.info.id} resData={resturant}/>)
