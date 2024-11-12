d1ee4ec2b65a25229ad99a7879d40128ddaba5db
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-11-12 23:11:34 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -5,14 +5,12 @@ import Header from "./components/Header.js";
 
   
 
- 
- 
   
   const AppLayout = ()=>{
     return (
         <div className="app">
      <Header />
-     <Body/>
+    
 
         </div>
     );
