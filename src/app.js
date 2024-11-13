011764ac6afbf700f2f8580789d2363185f4afb4
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-11-13 20:14:20 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import Header from "./components/Header.js";
     return (
         <div className="app">
      <Header />
-    
+     <Body/>
 
         </div>
     );
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 export  const CDN_URL=
  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/FOOD_CATALOG/IMAGES/CMS/2024/4/17/853b95b2-89fb-46ff-84c7-eaca466b882a_d8df1d76-10d4-4bc4-9dd2-cfc1dd5155cc.jpg" 
 
- export const LOGO_URL="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+ export const LOGO_URL="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-app-icon-food-explorer-design-template-8ae900e41ccbc0a2e1b48a85d239e389_screen.jpg?ts=1585237320"
 
 //named or default
