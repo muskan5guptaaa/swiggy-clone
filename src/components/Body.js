a9d8d0bb88b8faeb8b448d9f7a29e141acac6dcc
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-11-15 23:58:27 +0530

    commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,11 @@
 
 import ResturantCard from "./ResturantCard";
-import{useState,useEffect} from "react"
+import{useState} from "react"
 
-
+import resList from "../utils/mockData";
 const Body=()=>{
-  const [listOfResturants,setListOfResturant]=useState([]);
+  const [listOfResturants,setListOfResturant]=useState(resList);
 
-   useEffect(()=>{
-   fetchData();
-   },[]);
-
-const fetchData= async()=>{
-  const data = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3174112&lng=82.9738892&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING" 
-   )
-  const json= await data.json();
-  console.log(json); 
-}
   return(
  <div className="body">
 <div className="filter">
@@ -25,9 +14,9 @@ const fetchData= async()=>{
     onClick={()=>{
     //filter logic here
     const filteredList=listOfResturants.filter(
-      (res)=>res.data.avgRating > 4
+      (res)=>res.info.avgRating > 4
     );
-    setListOfResturant(filteredList)
+    setListOfResturant(filteredList);
     }}
   
     >
@@ -35,9 +24,9 @@ const fetchData= async()=>{
         </button>
 </div>
 <div className="res-container">
-  { listOfResturants.map(resturant=>
-  <ResturantCard key={resturant.info.id} resData={resturant}/>)
-  }
+  { listOfResturants.map((resturant)=>(
+  <ResturantCard key={resturant.info.id} resData={resturant}/>
+))}
 </div>
       </div>
     )
