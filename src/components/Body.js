2e0b9fcf34f424f5fb6d0592eb694d1ec5a030df
muskan5guptaaa <165368032+muskan5guptaaa@users.noreply.github.com>
2024-11-24 19:56:52 +0530

    body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,28 +1,46 @@
 
 import ResturantCard from "./ResturantCard";
-import{useEffect, useState} from "react"
-
-import resList from "../utils/mockData";
+import{useEffect, useState} from "react";
+import Shimmer from "./Shimmer";
 import { json } from "react-router-dom";
 const Body=()=>{
-  const [listOfResturants,setListOfResturant]=useState(resList);
-
+  const [listOfResturants,setListOfResturant]=useState([]);
+const [searchText,setSearchText]=useState("")
    useEffect(()=>{
     fetchData();
    },[]); 
 
    const fetchData = async () => {
-    const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3174112&lng=82.9738892&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
-      { mode: "no-cors" }
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.3174112&lng=82.9738892&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+         
     );
-  
-console.log(response)
+    const json=await data.json();
+    console.log(json);
+     setListOfResturant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   };
+
+  //Conditional Rendering
+    if(listOfResturants.length===0){
+    return <Shimmer/>
+  }
+
+ 
   
-     return(
+     return  listOfResturants.length===0 ? (
+     <Shimmer/>):(
  <div className="body">
 <div className="filter">
+  <div className="search">
+    <input type="text" className="search-box" value={searchText} onChange={(e)=>{
+      setSearchText(e.target.value)
+    }}/>
+    <button onClick={()=>{
+      //Filter the resturant cars and update ui
+      //searchText
+      console.log(searchText)
+    }}>Search</button>
+  </div>
     <button 
     className="filter-btn" 
     onClick={()=>{
