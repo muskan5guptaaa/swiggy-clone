ba5f97f6cecf46be2267336f83c8277052b888be
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-01-22 22:27:57 +0530

    commit

--- src/components/Error.js
+++ src/components/Error.js
@@ -5,7 +5,9 @@ const Error = () => {
     console.log(err)
   return (
     <div>
-        <h1>Oops!!!</h1><h3>{err.status}:{err.statusText}</h3>
+        <h1>Oops!!!</h1>
+        <h2>Helooo Muskan</h2>
+        <h3>{err.status}:{err.statusText}</h3>
     </div>
   )
 }
