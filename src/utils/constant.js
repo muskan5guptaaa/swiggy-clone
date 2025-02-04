a6cd1a009100e5419edf07fae4712b58fcd9ac00
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-02-04 22:56:20 +0530

    commit

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,4 +3,3 @@ export  const CDN_URL=
 
  export const LOGO_URL="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/food-app-icon-food-explorer-design-template-8ae900e41ccbc0a2e1b48a85d239e389_screen.jpg?ts=1585237320"
 
-//named or default
